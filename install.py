7115c3cbe69e1d5f9401835ccd1000d55dd75a16
Anirudh G <anirudhyamunang@gmail.com>
2021-02-28 19:02:17 +0100

    updated install.py

--- install.py
+++ install.py
@@ -4,12 +4,15 @@ def install():
   try:
     from pylint import epylint as lint
   except:
-    _in = input("pylint is not installed.\nDo you want to install it? [Y/n]")
+    _in = input("Pylint is not installed.\nDo you want to install it? [Y/n] ")
     if _in == "y" or \
       _in == "Y" or \
       _in == "":
       os.system("pip install pylint")
-      print("\n\nDone.")
+      print("\n\nDone\n\n.")
+
+  print("Pylint is installed")
+  os.system("pylint --version")
 
 if __name__ == "__main__":
   install()
