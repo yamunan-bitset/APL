0ec1a83cc2247da713a527a5da99552007898e8f
Anirudh G <anirudhyamunang@gmail.com>
2021-02-28 18:37:51 +0100

    added getAllDir

--- APL/__init__.py
+++ APL/__init__.py
@@ -9,32 +9,3 @@ except:
     _in == "":
     os.system("pip install pylint")
     print("\n\nDone.")
-
-def getStdout(_file: str) -> str:
-  """
-  This is used to get the stdout of a file.
-  
-  E.g.:
-  getStdout("main.py")
-  """
-  return lint.py_run(_file, return_std=True)[0]
-
-def getErr(_file: str) -> str:
-  """
-  This is to get the error in one file.
-
-  E.g.:
-  getErr("main.py")
-  """
-  return lint.py_run(_file, return_std=True)[1]
-  
-def getAll(_file: str):
-  """
-  This is for multiple files.
-  It will return the output of the terminal command:
-  $ pylint -j <number of files> <files>
-
-  E.g.:
-  getAll(["main.py", "test.py"])
-  """
-  print(os.popen("pylint -j 1 " + _file).read())
---
+++ APL/apl.py
@@ -0,0 +1,44 @@
+import os
+
+def getStdout(_file: str) -> str:
+  """
+  This is used to get the stdout of a file.
+  
+  E.g.:
+  getStdout("main.py")
+  """
+  return lint.py_run(_file, return_std=True)[0]
+
+def getErr(_file: str) -> str:
+  """
+  This is to get the error in one file.
+
+  E.g.:
+  getErr("main.py")
+  """
+  return lint.py_run(_file, return_std=True)[1]
+  
+def getAllFile(_file: str):
+  """
+  This is for multiple files.
+  It will return the output of the terminal command:
+  $ pylint -j <number of files> <files>
+
+  E.g.:
+  getAllFile("main.py test.py")
+  """
+  print(os.popen("pylint -j 1 " + _file).read())
+
+def getAllDir(_dir: str = "."):
+  """
+  This will find all python files from a 
+  specified directory or simple the current 
+  working directory.
+
+  E.g.:
+  getAllDir()
+  """
+  for f in os.listdir(_dir):
+    if f.endswith(".py"):
+      print("Found", f)
+      getAllFile(f)
